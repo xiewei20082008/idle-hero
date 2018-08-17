7058595100815ae50d2d6adddd1024e9832f4121
Wei Xie <xiewei.fire@gmail.com>
2018-08-17 17:31:25 +0800

    change code layout
    Signed-off-by: Wei Xie <xiewei.fire@gmail.com>

--- go/cv/cv.go
+++ go/cv/cv.go
@@ -1,12 +1,10 @@
-package main
+package cv
 
 import (
 	"errors"
 	"fmt"
 	"image"
 	"image/color"
-	"image/png"
-	"os"
 )
 
 var GAP = 16
@@ -81,7 +79,7 @@ func calDiff(img, pattern image.Image, x, y int) (float64, error) {
 	return float64(sum) / (float64(validPointNum) * 3.0 * 255.0), nil
 }
 
-func match(img, pattern image.Image, x1, y1, x2, y2 int) (float64, int, int) {
+func Match(img, pattern image.Image, x1, y1, x2, y2 int) (float64, int, int) {
 	patternDx := pattern.Bounds().Dx()
 	patternDy := pattern.Bounds().Dy()
 	imgDx := x2
@@ -111,65 +109,8 @@ func match(img, pattern image.Image, x1, y1, x2, y2 int) (float64, int, int) {
 	return diff, targetX, targetY
 }
 
-func completeMatch(img, pattern image.Image) (float64, int, int) {
+func CompleteMatch(img, pattern image.Image) (float64, int, int) {
 	imgDx := img.Bounds().Dx()
 	imgDy := img.Bounds().Dy()
-	return match(img, pattern, 0, 0, imgDx, imgDy)
-}
-
-type result struct {
-	pos    int
-	isCoin bool
-}
-
-func checkExist(img, pattern, coin image.Image) []result {
-	var results []result
-	picX := [4]int{473, 752, 1033, 1317}
-	picY := [2]int{541, 794}
-	picSizeX, picSizeY := 124, 123
-
-	coinX := [4]int{441, 724, 1004, 1286}
-	coinY := [2]int{690, 949}
-	coinSizeX, coinSizeY := 66, 53
-
-	index := -1
-
-	for i, vi := range picX {
-		for j, vj := range picY {
-			index++
-			similarity, _, _ := match(img, pattern, vi, vj, vi+picSizeX, vj+picSizeY)
-			// fmt.Printf("index %v similarity is %v\n", index, similarity)
-			if similarity >= 0.08 {
-				continue
-			}
-			similarity, _, _ = match(img, coin, coinX[i], coinY[j], coinX[i]+coinSizeX, coinY[j]+coinSizeY)
-			if DEBUG {
-				fmt.Printf("Find pattern in position %v\n", index)
-				fmt.Printf("Coin similarity is %v\n", similarity)
-			}
-			if similarity < 0.08 {
-				results = append(results, result{index, true})
-			} else {
-				results = append(results, result{index, false})
-			}
-		}
-	}
-	return results
-}
-
-func main() {
-	fileCoin, _ := os.Open("/sdcard/go/coin1.png")
-	imgCoin, _ := png.Decode(fileCoin)
-
-	file1, _ := os.Open(os.Args[1])
-	file2, _ := os.Open(os.Args[2])
-	defer file1.Close()
-	defer file2.Close()
-	img1, _ := png.Decode(file1)
-	img2, _ := png.Decode(file2)
-
-	results := checkExist(img1, img2, imgCoin)
-	for _, i := range results {
-		fmt.Printf("%v %v|", i.pos, i.isCoin)
-	}
+	return Match(img, pattern, 0, 0, imgDx, imgDy)
 }
---
+++ go/market.go
@@ -0,0 +1,69 @@
+package market
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+
+	"./cv"
+)
+
+var DEBUG = false
+
+type result struct {
+	pos    int
+	isCoin bool
+}
+
+func checkExist(img, pattern, coin image.Image) []result {
+	var results []result
+	picX := [4]int{473, 752, 1033, 1317}
+	picY := [2]int{541, 794}
+	picSizeX, picSizeY := 124, 123
+
+	coinX := [4]int{441, 724, 1004, 1286}
+	coinY := [2]int{690, 949}
+	coinSizeX, coinSizeY := 66, 53
+
+	index := -1
+
+	for i, vi := range picX {
+		for j, vj := range picY {
+			index++
+			similarity, _, _ := cv.Match(img, pattern, vi, vj, vi+picSizeX, vj+picSizeY)
+			// fmt.Printf("index %v similarity is %v\n", index, similarity)
+			if similarity >= 0.08 {
+				continue
+			}
+			similarity, _, _ = cv.Match(img, coin, coinX[i], coinY[j], coinX[i]+coinSizeX, coinY[j]+coinSizeY)
+			if DEBUG {
+				fmt.Printf("Find pattern in position %v\n", index)
+				fmt.Printf("Coin similarity is %v\n", similarity)
+			}
+			if similarity < 0.08 {
+				results = append(results, result{index, true})
+			} else {
+				results = append(results, result{index, false})
+			}
+		}
+	}
+	return results
+}
+
+func main() {
+	fileCoin, _ := os.Open("/sdcard/go/coin1.png")
+	imgCoin, _ := png.Decode(fileCoin)
+
+	file1, _ := os.Open(os.Args[1])
+	file2, _ := os.Open(os.Args[2])
+	defer file1.Close()
+	defer file2.Close()
+	img1, _ := png.Decode(file1)
+	img2, _ := png.Decode(file2)
+
+	results := checkExist(img1, img2, imgCoin)
+	for _, i := range results {
+		fmt.Printf("%v %v|", i.pos, i.isCoin)
+	}
+}
