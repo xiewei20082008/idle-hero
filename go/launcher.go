0a901299736c3d425bd1dfa051460db6bab0ca07
Wei Xie <xiewei.fire@gmail.com>
2018-09-14 04:20:01 +0800

    New way to mayi launcher
    Signed-off-by: Wei Xie <xiewei.fire@gmail.com>

--- go/launcher.go
+++ go/launcher.go
@@ -3,12 +3,8 @@ package main
 import (
 	"fmt"
 	"image"
-	"image/png"
-	"os"
 	"os/exec"
 	"time"
-
-	"./cv"
 	"github.com/jehiah/go-strftime"
 )
 
@@ -37,59 +33,63 @@ func main() {
 	exec.Command("sh","-c", "monkey -p com.touchsprite.android -c android.intent.category.LAUNCHER 1").Run()
 	fmt.Printf("after exe monkey\n")
 	time.Sleep(12 * time.Second)
+	screencap()
+	click(1020,460)
+	screencap()
+	time.Sleep(5 * time.Second)
+	click(945,588)
 	
 
-	notRunningFloaterFile, _ := os.Open("/sdcard/go/not_running.png")
-	notRunningFloaterPattern, _ := png.Decode(notRunningFloaterFile)
-	defer notRunningFloaterFile.Close()
+	// notRunningFloaterFile, _ := os.Open("/sdcard/go/not_running.png")
+	// notRunningFloaterPattern, _ := png.Decode(notRunningFloaterFile)
+	// defer notRunningFloaterFile.Close()
 
-	runningFloaterFile, _ := os.Open("/sdcard/go/running.png")
-	runningFloaterPattern, _ := png.Decode(runningFloaterFile)
-	defer runningFloaterFile.Close()
+	// runningFloaterFile, _ := os.Open("/sdcard/go/running.png")
+	// runningFloaterPattern, _ := png.Decode(runningFloaterFile)
+	// defer runningFloaterFile.Close()
 
-	stopFile, _ := os.Open("/sdcard/go/stop.png")
-	stopPattern, _ := png.Decode(stopFile)
-	defer stopFile.Close()
+	// stopFile, _ := os.Open("/sdcard/go/stop.png")
+	// stopPattern, _ := png.Decode(stopFile)
+	// defer stopFile.Close()
 
-	name := screencap()
-	time.Sleep(8 * time.Second)
-	file1, _ := os.Open(name)
-	defer file1.Close()
+	// time.Sleep(8 * time.Second)
+	// file1, _ := os.Open(name)
+	// defer file1.Close()
 
-	img1, _ := png.Decode(file1)
+	// img1, _ := png.Decode(file1)
 
-	for {
+	// for {
 
-		sim, x, y := cv.Match(img1, notRunningFloaterPattern, 0, 0, 117, 2248)
-		if sim < 0.08 {
-			sim1, _, _ := cv.Match(img1, stopPattern, 170, 0, 320, 2248)
-			if sim1 < 0.08 {
-				click(x+100, y)
-			} else {
-				click(x, y)
-				time.Sleep(2 * time.Second)
-				click(x+100, y)
-			}
-			break
-		}
+	// 	sim, x, y := cv.Match(img1, notRunningFloaterPattern, 0, 0, 117, 2248)
+	// 	if sim < 0.08 {
+	// 		sim1, _, _ := cv.Match(img1, stopPattern, 170, 0, 320, 2248)
+	// 		if sim1 < 0.08 {
+	// 			click(x+100, y)
+	// 		} else {
+	// 			click(x, y)
+	// 			time.Sleep(2 * time.Second)
+	// 			click(x+100, y)
+	// 		}
+	// 		break
+	// 	}
 
-		sim, x, y = cv.Match(img1, runningFloaterPattern, 0, 0, 117, 2248)
-		if sim < 0.08 {
-			sim1, p, q := cv.Match(img1, stopPattern, 170, 0, 320, 2248)
-			if sim1 < 0.08 {
-				click(p, q)
-				time.Sleep(6 * time.Second)
-				click(x+100, y)
-			} else {
-				click(x, y)
-				time.Sleep(2 * time.Second)
-				click(x+200, y)
-				time.Sleep(6 * time.Second)
-				click(x+100, y)
-			}
-			break
-		}
+	// 	sim, x, y = cv.Match(img1, runningFloaterPattern, 0, 0, 117, 2248)
+	// 	if sim < 0.08 {
+	// 		sim1, p, q := cv.Match(img1, stopPattern, 170, 0, 320, 2248)
+	// 		if sim1 < 0.08 {
+	// 			click(p, q)
+	// 			time.Sleep(6 * time.Second)
+	// 			click(x+100, y)
+	// 		} else {
+	// 			click(x, y)
+	// 			time.Sleep(2 * time.Second)
+	// 			click(x+200, y)
+	// 			time.Sleep(6 * time.Second)
+	// 			click(x+100, y)
+	// 		}
+	// 		break
+	// 	}
 
-		time.Sleep(2 * time.Second)
-	}
+	// 	time.Sleep(2 * time.Second)
+	// }
 }
